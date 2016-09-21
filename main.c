b6d2e2a48318f6d551fb11be5bd49f124213a27b
hugolino <hdecaux@e1r8p9.42.fr>
2016-09-21 13:26:49 +0200

    remove tab

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: hdecaux <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/11/27 11:18:58 by hdecaux           #+#    #+#             */
-/*   Updated: 2016/01/27 12:06:58 by rthys            ###   ########.fr       */
+/*   Updated: 2016/09/21 13:26:40 by hdecaux          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,7 @@ int		main(int argc, char **argv)
 		fd = open(argv[1], O_RDONLY);
 		ret = read(fd, buf, BUFF_SIZE);
 		tab = ft_bufcpy(buf);
-		ft_puttabchar(tab, buf);
+		ft_puttab(tab, buf);
 		ft_errors(ret, buf, tab);
 		doitall(buf);
 	}
