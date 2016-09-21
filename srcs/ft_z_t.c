f6b8ab5160824b65671cd90d04b068ed7a0cc800
Robin THYS <rthys@e1r11p8.42.fr>
2016-09-21 16:06:11 +0200

    Tests square

--- srcs/ft_z_t.c
+++ srcs/ft_z_t.c
@@ -6,7 +6,7 @@
 /*   By: hdecaux <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/03 10:38:58 by hdecaux           #+#    #+#             */
-/*   Updated: 2016/09/21 15:24:53 by rthys            ###   ########.fr       */
+/*   Updated: 2016/09/21 16:05:57 by rthys            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,8 @@ char	*ft_is_z_up(char **tab, char *buf, int *pieces)
 		while (j < 6)
 		{
 			if (tab[i][j] == '#' && tab[i + 1][j - 1] == '#' \
-			&& tab[i + 1][j] == '#' && tab[i + 2][j - 1] == '#')
+			&& tab[i + 1][j] == '#' && tab[i + 2][j - 1] == '#'\
+			&& ft_strcmp(ft_is_square(tab, buf, pieces), "\n##\n##\n") != 0)
 			{
 				pieces += 5;
 				return ("\n.#\n##\n#.\n");
@@ -48,7 +49,8 @@ char	*ft_is_t(char **tab, char *buf, int *pieces)
 		while (j < 6)
 		{
 			if (tab[i][j] == '#' && tab[i][j + 1] == '#' \
-			&& tab[i][j + 2] == '#' && tab[i + 1][j + 1] == '#')
+			&& tab[i][j + 2] == '#' && tab[i + 1][j + 1] == '#'\
+			&& ft_strcmp(ft_is_square(tab, buf, pieces), "\n##\n##\n") != 0)
 			{
 				pieces += 5;
 				return ("\n###\n.#.\n");
@@ -72,7 +74,8 @@ char	*ft_is_t_left(char **tab, char *buf, int *pieces)
 		while (j < 6)
 		{
 			if (tab[i][j] == '#' && tab[i + 1][j - 1] == '#' \
-			&& tab[i + 1][j] == '#' && tab[i + 2][j] == '#')
+			&& tab[i + 1][j] == '#' && tab[i + 2][j] == '#'\
+			&& ft_strcmp(ft_is_square(tab, buf, pieces), "\n##\n##\n") != 0)
 			{
 				pieces += 5;
 				return ("\n.#\n##\n.#\n");
@@ -96,7 +99,8 @@ char	*ft_is_t_right(char **tab, char *buf, int *pieces)
 		while (j < 6)
 		{
 			if (tab[i][j] == '#' && tab[i + 1][j] == '#' \
-				&& tab[i + 1][j + 1] == '#' && tab[i + 2][j] == '#')
+			&& tab[i + 1][j + 1] == '#' && tab[i + 2][j] == '#'\
+			&& ft_strcmp(ft_is_square(tab, buf, pieces), "\n##\n##\n") != 0)
 			{
 				pieces += 5;
 				return ("\n#.\n##\n#.\n");
@@ -120,7 +124,8 @@ char	*ft_is_t_up(char **tab, char *buf, int *pieces)
 		while (j < 6)
 		{
 			if (tab[i][j] == '#' && tab[i + 1][j - 1] == '#' \
-				&& tab[i + 1][j] == '#' && tab[i + 1][j + 1] == '#')
+			&& tab[i + 1][j] == '#' && tab[i + 1][j + 1] == '#'\
+			&& ft_strcmp(ft_is_square(tab, buf, pieces), "\n##\n##\n") != 0)
 			{
 				pieces += 5;
 				return ("\n.#.\n###\n");
