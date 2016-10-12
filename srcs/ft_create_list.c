e933672fb36a9390f80c8cf6774dbdf1dd290e89
Robin Thys <Thys@MacBook-Pro-de-Robin.local>
2016-10-12 19:02:11 +0200

    Ajout gestion de liste

---
+++ srcs/ft_create_list.c
@@ -0,0 +1,94 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_manipulate_list.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rthys <rthys@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/10/12 14:27:13 by rthys             #+#    #+#             */
+/*   Updated: 2016/10/12 19:00:11 by rthys            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "fillit.h"
+#include <stdio.h>
+
+t_etri	*ft_add_list(t_coord *cd, t_etri *tetri, size_t i, size_t *j)
+{
+	t_etri	*new_t;
+	size_t	k;
+
+	k = 0;
+	new_t = (t_etri *)malloc(sizeof(t_etri));
+	new_t->rank = i + 1;
+	new_t->let = tetri->let + 1;
+	new_t->previous = tetri;
+	new_t->tetri = (char **)malloc(sizeof(char *) * 6);
+	while (TAB[*j][0] != '\n' && TAB[*j][0] != '0')
+	{
+		new_t->tetri[k] = (char *)malloc(sizeof(char) * 6);
+		new_t->tetri[k] = ft_cpy_tetri(cd, j);
+		k++;
+		new_t->len = k;
+	}
+	*j = *j + 1;
+	if (new_t->rank == NB_P - 2)
+		new_t->next = NULL;
+	NEXT = new_t;
+	tetri = new_t;
+	return (tetri);
+}
+
+t_etri	*ft_make_list(t_coord *cd)
+{
+	t_etri	*tetri;
+	t_etri	*begin_list;
+	size_t	i;
+	size_t	*j;
+
+	i = 0;
+	j = (size_t *)malloc(sizeof(size_t));
+	*j = 0;
+	while (i < NB_P)
+	{
+		if (i == 0)
+		{
+			tetri = (t_etri *)malloc(sizeof(t_etri));
+			RANK = i;
+			tetri->let = 'A';
+			PREV = NULL;
+			ft_cpy_first(cd, tetri, j);
+			begin_list = tetri;
+		}
+		else
+			tetri = ft_add_list(cd, tetri, i, j);
+		i++;
+	}
+	tetri = begin_list;
+	return (tetri);
+}
+
+char	*ft_cpy_tetri(t_coord *cd, size_t *j)
+{
+	char	*str;
+
+	str = ft_strdup(TAB[*j]);
+	*j = *j + 1;
+	return (str);
+}
+
+void		ft_cpy_first(t_coord *cd, t_etri *tetri, size_t *j)
+{
+	size_t	k;
+
+	k = 0;
+	TETRI = (char **)malloc(sizeof(char *) * 5);
+	while (TAB[*j][0] != '\n' && TAB[*j][0] != '0')
+	{
+		TETRI[k] = (char *)malloc(sizeof(char) * 6);
+		TETRI[k] = ft_cpy_tetri(cd, j);
+		k++;
+	}
+	LEN = k;
+	*j = *j + 1;
+}
---
+++ srcs/ft_resolve.c
@@ -0,0 +1,151 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_map_creator.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rthys <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/09/22 16:35:48 by rthys             #+#    #+#             */
+/*   Updated: 2016/10/12 15:56:44 by rthys            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "fillit.h"
+#include <stdio.h>
+
+char	**ft_map_creator(size_t cotes)
+{
+	char	**map;
+	size_t	i;
+	size_t	j;
+
+	i = 0;
+	map = (char **)malloc(sizeof(char *) * (cotes));
+	while (i < cotes)
+	{
+		map[i] = (char *)malloc(sizeof(char) * (cotes) + 1);
+		i++;
+	}
+	i = 0;
+	while (i < cotes)
+	{
+		j = 0;
+		while (j < cotes)
+		{
+			map[i][j] = '.';
+			j++;
+		}
+		map[i][j] = '\n';
+		map[i][j + 1] = '\0';
+		i++;
+	}
+	return (map);
+}
+
+void	ft_place_tetri(t_coord *cd)
+{
+	int		count;
+
+	count = 0;
+	X_FIRST = X_P;
+	Y_FIRST = Y_P;
+	SAVE = ft_cpy_tab(MAP, cd);
+	while (count < 4 && TAB[Y_P][0] != '0')
+	{
+		if (NEWYM >= COTE)
+		{
+			MAP = SAVE;
+			Y_P = Y_FIRST;
+			X_P = X_FIRST;
+			return ;
+		}
+		while (TAB[Y_P][X_P]  && count < 4)
+		{
+			if (TAB[Y_P][X_P] == '#' && MAP[NEWYM][NEWXM] == '.')
+			{
+				MAP[NEWYM][NEWXM] = LET;
+				count++;
+			}
+			else if ((TAB[Y_P][X_P] == '#' && MAP[NEWYM][NEWXM] != '.'))
+			{
+				X_P = X_FIRST;
+				Y_P = Y_FIRST;
+				MAP = SAVE;
+				return ;
+			}
+			X_P++;
+		}
+		Y_P++;
+		X_P = 0;
+	}
+	LET++;
+	Y_M = NEWYM;
+	X_M = NEWXM;
+}
+
+void	ft_prepare_algo(t_coord *cd)
+{
+	X_M = 0;
+	Y_M = 0;
+	X_P = 0;
+	Y_P = 0;
+	LET = 'A';
+	MAP = ft_map_creator(COTE);
+	ft_resolve(cd);
+}
+
+void	ft_resolve(t_coord *cd)
+{
+	Y_M = 0;
+	while (Y_M < COTE && TABP != '0')
+	{
+		X_M = 0;
+		while (Y_M < COTE && X_M < COTE && TABP != '0' && X_P < ft_strlen(TAB[Y_P]))
+		{
+			if (TABP == '\n' && Y_P != 0)
+			{
+				Y_P++;
+				X_P = 0;
+				ft_resolve(cd);
+				return ;
+			}
+			if (MAP[Y_M][X_M] == '.' && TABP != '0')
+			{
+				if (MAP[Y_M][X_M] == '.' && TABP != '\n' && TABP != '0')
+				{
+					if (ft_dota(MAP[Y_M], X_M) >= ft_hash(TAB[Y_P], X_P))
+						if (TABP == '#')
+							ft_place_tetri(cd);
+					if (TABP == '.')
+						X_P++;
+					if (X_M + 1 >= COTE && Y_M + 1 < COTE)
+					{
+						X_M = 0;
+						Y_M++;
+					}
+					else
+						X_M++;
+				}
+			}
+			else if (MAP[Y_M][X_M] >= 'A' && MAP[Y_M][X_M] <= 'Z')
+				X_M++;
+		}
+		if (TABP != '0' && X_P == ft_strlen(TAB[Y_P]) - 1)
+		{
+			X_P = 0;
+			Y_P++;
+		}
+		if ((X_M == COTE) || (Y_M + 1 < COTE))
+			Y_M++;
+	}
+	if (Y_P != NB_L)
+	{
+		COTE++;
+		X_P = 0;
+		Y_P = 0;
+		LET = 'A';
+		MAP = ft_map_creator(COTE);
+		ft_resolve(cd);
+	}
+	return ;
+}
